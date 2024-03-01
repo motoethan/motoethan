e4b30f2831808ebddf15843f1b05d1c2cb51d8a6
motoethan <66018445+motoethan@users.noreply.github.com>
2024-03-01 20:08:14 +0300

    Add files via upload

---
+++ donate.py
@@ -0,0 +1,15 @@
+from donationalerts.asyncio_api import Alert
+import requests
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+alert = Alert("i7xLtifVTseTU3nUCxfZ")
+
+print('Система приёма донатов запущена');
+
+@alert.event()
+async def handler(event):
+    print(f"{event.username} пожертвовал {event.amount}")
+    payload = {'id_user': event.username, 'summ': event.amount}
+    requests.get('https://mtchat.site/memecoin_bot/donate.php', params=payload)
