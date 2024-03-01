a213f9185be1bf750aed9b89198eb44ee8cab24a
motoethan <66018445+motoethan@users.noreply.github.com>
2024-03-01 20:15:21 +0300

    Update donate.py

--- donate.py
+++ donate.py
@@ -6,10 +6,10 @@ sys.stdout.reconfigure(encoding='utf-8')
 
 alert = Alert("i7xLtifVTseTU3nUCxfZ")
 
-print('Система приёма донатов запущена');
+print('The donation acceptance system is running');
 
 @alert.event()
 async def handler(event):
-    print(f"{event.username} пожертвовал {event.amount}")
+    print(f"{event.username} donate {event.amount}")
     payload = {'id_user': event.username, 'summ': event.amount}
     requests.get('https://mtchat.site/memecoin_bot/donate.php', params=payload)
